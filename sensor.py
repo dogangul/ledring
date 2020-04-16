491a125ed5b208e2e7be052f455c553c4309d548
dogangul <dogan.gul@evbox.com>
2020-04-16 19:13:28 +0200

    syntax

--- sensor.py
+++ sensor.py
@@ -9,8 +9,8 @@ VCNL4200_DeviceID_REG = 0x0E
 def sensor_exists():
     read = bus.read_word_data(device_address,0x0E)
     print(read)
-	if (read==0x1058):
-		return true
+    if read==0x1058:
+        return true
 	else:
 	return false
 
