fa535e994890a9c1abef0d00c646d93bcc2e0816
dogangul <dogan.gul@evbox.com>
2020-04-16 19:08:48 +0200

    sensor found

--- sensor.py
+++ sensor.py
@@ -9,16 +9,15 @@ VCNL4200_DeviceID_REG = 0x0E
 def sensor_exists():
     read = bus.read_word_data(device_address,0x0E)
     print(read)
-	#if ((lowByte == 0x58) && (highByte == 0x10)) {
-	#	return true;
-	#}
-	#return false
+	if (read==0x1058):
+		return true
+	else:
+	return false
 
 try:
     print("\nSensor is initializing.")
-    sensor_exists()
-    #if (response==1):
-        #print("VCNL4200 found")
+    if (sensor_exists()):
+        print("VCNL4200 found")
 		#sensor_initialize()
 		#print("VCNL4200 initialized")
 
