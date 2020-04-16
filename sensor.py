e10ff40a24ee4d8fbf711055d1b6739fe2a1ab35
dogangul <dogan.gul@evbox.com>
2020-04-16 19:56:11 +0200

    conf3

--- sensor.py
+++ sensor.py
@@ -8,18 +8,20 @@ device_address = 0x51
 
 
 def set_ALS_CONF(val):
-    bus.write_word_data(device_address,0x00,val)
+   bus.write_word_data(device_address,0x00,val)
 
 def set_PS_CONF1_CONF2(val):
-    bus.write_word_data(device_address,0x03,val)
+   bus.write_word_data(device_address,0x03,val)
 
+def set_PS_CONF3_MS(val):
+   bus.write_word_data(device_address,0x04,val)   
 def sensor_exists():
     return (bus.read_word_data(device_address,0x0E) == 0x1058)
 
 def initialize():
    set_ALS_CONF(0B01000000) #Edit the binary settings here to change default startup options
    set_PS_CONF1_CONF2(0B0000101100101010)
-	#set_PS_CONF3_MS();
+   set_PS_CONF3_MS(0B0000011101110000)
 
 	#Set the PS interrupt levels
 	#write16_LowHigh(VCNL4200_PS_THDL_REG, B10001000, B00010011);
@@ -30,7 +32,7 @@ try:
     if (sensor_exists()):
         print("VCNL4200 found")
     initialize()
-    print(bus.read_word_data(device_address,0x03))
+    print(bus.read_word_data(device_address,0x04))
         # print("VCNL4200 initialized")
 
 finally:
