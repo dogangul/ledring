67da9eaf5e3c17658fe2178979151ddd531bbd90
dogangul <dogan.gul@evbox.com>
2020-04-16 19:48:45 +0200

    initialize

--- sensor.py
+++ sensor.py
@@ -4,17 +4,32 @@ import time
 
 bus = smbus.SMBus(1)
 device_address = 0x51
-VCNL4200_DeviceID_REG = 0x0E
+
+
+
+def set_ALS_CONF(val):
+    bus.write_word_data(device_address,0x00,val)
 
 def sensor_exists():
-    print(bus.read_word_data(device_address,0x0E))
     return (bus.read_word_data(device_address,0x0E) == 0x1058)
+
+def initialize():
+    set_ALS_CONF(0B01000000) #Edit the binary settings here to change default startup options
+
+	#set_PS_CONF1_CONF2();
+	#set_PS_CONF3_MS();
+
+	#Set the PS interrupt levels
+	#write16_LowHigh(VCNL4200_PS_THDL_REG, B10001000, B00010011);
+	#write16_LowHigh(VCNL4200_PS_THDH_REG, B11100000, B00101110);
+
 try:
-    print("\nSensor is initializing.")
+    print("\nI2C is initializing.")
     if (sensor_exists()):
         print("VCNL4200 found")
-		#sensor_initialize()
-		#print("VCNL4200 initialized")
+    initialize()
+    print(bus.read_word_data(device_address,0x00))
+        # print("VCNL4200 initialized")
 
 finally:
     print("\nSensor is terminating.")
