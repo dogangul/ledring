306a48662c7e5ff6739e4def69fc39d7d286b8fb
dogangul <dogan.gul@evbox.com>
2020-04-16 19:04:50 +0200

    read_word_data works

--- sensor.py
+++ sensor.py
@@ -3,11 +3,11 @@ import RPi.GPIO as GPIO
 import time
 
 bus = smbus.SMBus(1)
-SENSOR_ADDR = 0x51
+device_address = 0x51
 VCNL4200_DeviceID_REG = 0x0E
 
 def sensor_exists():
-    read = bus.process_call(SENSOR_ADDR,VCNL4200_DeviceID_REG,SENSOR_ADDR)
+    read = bus.read_word_data(device_address,0x0E)
     print(read)
 	#if ((lowByte == 0x58) && (highByte == 0x10)) {
 	#	return true;
