eac8c23b148e5947c3844b02a66acb3441781460
dogangul <dogan.gul@evbox.com>
2020-04-17 08:34:49 +0200

    dim

--- ledring_prox.py
+++ ledring_prox.py
@@ -134,7 +134,7 @@ def ChasingEffect_Custom(prox,amb):
     if amb > 1000 :
         dim = 1
     else:
-        dim = 0.01
+        dim = 0.05
         
     if prox>1500:    
         LED_Color_Set(0,240*dim,0,0)
