ce5b32b10197928aac58f6038218c669945d87a6
Antonio Fontoura <antonio.fontoura@ev-box.com>
2020-05-16 21:00:08 +0200

    expose camera port

--- systemfiles/installServices.sh
+++ systemfiles/installServices.sh
@@ -5,3 +5,8 @@ cp ledring_server.service /lib/systemd/system/
 
 systemctl enable ledring_server.service
 systemctl start ledring_server.service
+
+cp serverexpose.service /lib/systemd/system/
+
+systemctl enable serverexpose.service
+systemctl start serverexpose.service
